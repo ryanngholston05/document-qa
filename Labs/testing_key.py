be4cd89eae1693d28faf4d3d47359f5346fd87c3
ryanngholston05 <rtgholst@syr.edu>
2026-01-30 00:19:22 +0000

    deleted

--- Labs/testing_key.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("My App Using Secrets")
-
-secret_key = st.secrets.API_KEY
-st.write("Here's my secret key:", secret_key)
-
-st.write("And here again:", st.secrets["API_KEY"])
-
